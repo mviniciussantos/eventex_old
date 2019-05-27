7986efb537d6587d22444cd3ca56f48c64f77e1e
Marcos Vinicius dos Santos <marcosvini99@gmail.com>
2019-05-27 00:05:37 -0300

    ajuste no parametro do banco

--- eventex/settings.py
+++ eventex/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'eventex.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 DATABASES = {
-    'default': {config('DATABASE_URL', default=default_dburl, cast=dburl)},
+    'default': config('DATABASE_URL', default=default_dburl, cast=dburl),
 }
 
 # Password validation
