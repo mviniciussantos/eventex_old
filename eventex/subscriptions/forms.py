132244a5370eeea94f1ecbea7f7aa9cbd12d36db
Marcos Vinicius dos Santos <marcosvini99@gmail.com>
2019-08-10 00:52:16 -0300

    Email ou telefone opcionais

--- eventex/subscriptions/forms.py
+++ eventex/subscriptions/forms.py
@@ -2,11 +2,6 @@ from django import forms
 from django.core.exceptions import ValidationError
 
 
-# class SubscriptionForm(forms.ModelForm):
-#     class Meta:
-#         model = Subscription
-#         fields = ['name', 'cpf', 'email', 'phone']
-
 def validate_cpf(value):
     if not value.isdigit():
         raise ValidationError('O Cpf deve ter somente números', 'digits')
@@ -18,10 +13,16 @@ def validate_cpf(value):
 class SubscriptionForm(forms.Form):
     name = forms.CharField(label='Nome')
     cpf = forms.CharField(label='CPF', validators=[validate_cpf])
-    email = forms.EmailField(label='Email')
-    phone = forms.CharField(label='Telefone')
+    email = forms.EmailField(label='Email', required=False)
+    phone = forms.CharField(label='Telefone', required=False)
 
     def clean_name(self):
         name = self.cleaned_data['name']
         words = [w.capitalize() for w in name.split()]
         return ' '.join(words)
+
+    def clean(self):
+        if not self.cleaned_data.get('email') and not self.cleaned_data.get('phone'):
+            raise ValidationError('Informe o e-mail ou o telefone')
+
+        return self.cleaned_data
