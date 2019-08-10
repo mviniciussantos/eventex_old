d217bd27592d370b7ef845285944bf5961405817
Marcos Vinicius dos Santos <marcosvini99@gmail.com>
2019-08-10 10:51:31 -0300

    Keynote speakers

--- eventex/core/views.py
+++ eventex/core/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, "index.html")
+    speakers = [
+        {'name': 'Grace Hopper', 'foto': 'http://hbn.link/hopper-pic'},
+        {'name': 'Alan Turing', 'foto': 'http://hbn.link/turing-pic'}
+    ]
+    return render(request, "index.html", {'speakers': speakers})
